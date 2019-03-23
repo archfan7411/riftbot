f22c841bc27ea5e460eea3a909b0b902b8e23005
archfan <33993466+archfan7411@users.noreply.github.com>
2019-03-23 14:00:47 -0400

    Add the rewritten commands

--- commands/cmd_8ball.py
+++ commands/cmd_8ball.py
@@ -1,7 +1,17 @@
 import random
-async def cmd_8ball(message):
-    channel = message.channel
+
+async def cmd_8ball(message, args):
+
     responses = ["Yep!", "Of course.", "Absolutely!", "Eh, ask again.", "Unsure", "What? No!", "I don't think so.", "Probably a bad idea."]
+
     response = random.choice(responses)
-    await send(channel, response)
+
+    random_embed = discord.Embed(
+        title = "**8ball**",
+        description = response,
+        colour = get_embed_color()
+    )
+
+    await message.channel.send(embed = random_embed)
+
 register_command("8ball", cmd_8ball)
